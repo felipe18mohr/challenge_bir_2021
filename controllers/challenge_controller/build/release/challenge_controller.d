c55f238dd2177540ae2619d73f76f68beb4acd12
Felipe Mohr <felipe18mohr@gmail.com>
2021-01-08 01:03:59 -0300

    update folders and controller created

---
+++ controllers/challenge_controller/build/release/challenge_controller.d
@@ -0,0 +1,7 @@
+build/release/challenge_controller.o: challenge_controller.c \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/led.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h
